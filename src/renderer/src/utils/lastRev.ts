485d9e76101f4fd0d98fec92d0b2f81b46adad06
matiash26 <matias.henri26@gmail.com>
2023-12-31 21:37:51 -0300

    fixed last rev

--- src/renderer/src/utils/lastRev.ts
+++ src/renderer/src/utils/lastRev.ts
@@ -1,5 +1,5 @@
 export default function lastRev(date: number): boolean {
   const dayRev = new Date(date).getDate()
   const today = new Date().getDate()
-  return today !== dayRev
+  return today !== dayRev || date === 0
 }
