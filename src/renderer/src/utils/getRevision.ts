05ffeadbb0096fc60d773a42acfd3d61a1cf6799
matiash26 <matias.henri26@gmail.com>
2023-12-31 22:19:28 -0300

    fixed bug to get rev

--- src/renderer/src/utils/getRevision.ts
+++ src/renderer/src/utils/getRevision.ts
@@ -1,12 +1,13 @@
 export default function getRevision(date: number): boolean {
-  const dataTimestamp = new Date(date)
+  if (date !== 0) {
+    const dataTimestamp = new Date(date)
+    const currentDate = new Date()
+    const sameDay =
+      dataTimestamp.getFullYear() <= currentDate.getFullYear() &&
+      dataTimestamp.getMonth() <= currentDate.getMonth() &&
+      dataTimestamp.getDate() <= currentDate.getDate()
 
-  const currentDate = new Date()
-
-  const sameDay =
-    dataTimestamp.getFullYear() <= currentDate.getFullYear() &&
-    dataTimestamp.getMonth() <= currentDate.getMonth() &&
-    dataTimestamp.getDate() <= currentDate.getDate()
-
-  return sameDay
+    return sameDay
+  }
+  return false
 }
